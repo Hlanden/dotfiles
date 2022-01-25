af833f9eb5cd3380d532e32a3f7f633dcaa3c048
Jørgen Haaland <jorghaal@stud.ntnu.no>
2022-01-25 13:57:59 +0100

    Make i3 config for two screens

--- i3/startup.sh
+++ i3/startup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-xrandr --output DVI-I-1 --auto --rotate normal --pos 0x0 --output DVI-D-0 --primary --auto --rotate normal --right-of DVI-I-1 --output HDMI-0 --rotate right --right-of DVI-D-0
+# xrandr --output DVI-I-1 --auto --rotate normal --pos 0x0 --output DVI-D-0 --primary --auto --rotate normal --right-of DVI-I-1 --output HDMI-0 --rotate right --right-of DVI-D-0
+xrandr --output DVI-D-0 --primary --auto --rotate normal --right-of DVI-I-1 --output HDMI-0 --rotate right --right-of DVI-D-0
 sleep 1
-picom -b --config ~/.dotfiles/picom/picom.conf &
+picom -b --config ~/.config/picom/picom.conf &
 feh --randomize --bg-fill ~/Pictures/backgrounds/* &
