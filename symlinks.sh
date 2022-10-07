197afe961d7c4db734dc39645f9df5734c89b5db
Jørgen Haaland <jorgen.haaland@outlook.com>
2022-10-07 22:50:19 +0200

    Uupdate symlinks script

---
+++ symlinks.sh
@@ -0,0 +1,15 @@
+# NOTE: Use at own risk. This will overwrite all existing symlinks
+mkdir -p ~/.config 
+
+ln -sf ~/dotfiles/bash/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/bash/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/nvim ~/.config/nvim
+ln -sf ~/dotfiles/tmux ~/.config/tmux
+ln -sf ~/dotfiles/ranger ~/.config/ranger
+ln -sf ~/dotfiles/ultisnips ~/.config/ultisnips
+
+# alacritty
+# TODO: i3
+# TODO: picom
+# TODO: polybar
+# TODO: ultisnips
