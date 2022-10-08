79da692dc13eb806deb1be112ea139953cf57cdd
Jørgen Haaland <jorgen.haaland@outlook.com>
2022-10-08 13:36:04 +0200

    Add config for lsp keybindings

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,7 +3,7 @@ require('Hlanden.settings')
 require('Hlanden.autocmd')
 require('Hlanden.plugins')
 require('Hlanden.key_bindings')
-require('Hlanden.cmp')
+require('Hlanden.lsp_and_cmp')
 
 ---Pretty print lua table
 function _G.dump(...)
