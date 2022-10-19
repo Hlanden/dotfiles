d8b000ddef5e64c4f5fbe85022378190365111db
Jorgen Haaland <jorgen.haaland@wheel.me>
2022-10-19 10:20:18 +0200

    Changes to neovim config. Added many new plugins/

--- nvim/lua/Hlanden/key_bindings.lua
+++ nvim/lua/Hlanden/key_bindings.lua
@@ -31,83 +31,99 @@ map('n', '<leader>es', ':CocCommand snippets.editSnippets<CR>')
 -- map(n, '<leader><', '`')
 
 -- Movement in insert mode
-map('i','<C-e>','<C-o>$')
-map('i','<C-a>','<C-o>0')
+map('i', '<C-e>', '<C-o>$')
+map('i', '<C-a>', '<C-o>0')
 
 -- Copy/paste/delete
-map('x','<leader>p','"_dP')
-map('n','<leader>p','"+p')
-map('v','<leader>p','"+p')
+map('x', '<leader>p', '"_dP')
+map('n', '<leader>p', '"+p')
+map('v', '<leader>p', '"+p')
 
-map('n','yp',':let,@",=,expand("%"),<cr>')
-map('n','<leader>y','"+y')
-map('v','<leader>y','"+y')
-map('n','<leader>Y','gg"+yG')
-map('n','Y','y$')
+map('n', 'yp', ':let,@",=,expand("%"),<cr>')
+map('n', '<leader>y', '"+y')
+map('v', '<leader>y', '"+y')
+map('n', '<leader>Y', 'gg"+yG')
+map('n', 'Y', 'y$')
 
 -- Delete without saving to register
-map('n','<leader>d','"_d')
-map('v','<leader>d','"_d')
+map('n', '<leader>d', '"_d')
+map('v', '<leader>d', '"_d')
 
 -- KEY-BINDINGS:
-map('n','n','nzzzv')
-map('n','N','Nzzzv')
-map('n','J','mzJ`z,')
+map('n', 'n', 'nzzzv')
+map('n', 'N', 'Nzzzv')
+map('n', 'J', 'mzJ`z,')
 
-map('i',',',',<c-g>u')
-map('i','.','.<c-g>u')
-map('i','!','!<c-g>u')
-map('i','?','?<c-g>u')
+map('i', ',', ',<c-g>u')
+map('i', '.', '.<c-g>u')
+map('i', '!', '!<c-g>u')
+map('i', '?', '?<c-g>u')
 
-map('v','J',":m,'>+1<CR>gv=gv")
-map('v','K',":m,'>-2<CR>gv=gv")
+map('v', 'J', ":m,'>+1<CR>gv=gv")
+map('v', 'K', ":m,'>-2<CR>gv=gv")
 
 -- Make ctrl-c work as esc
-map('i','<ESC>','<C-C>')
+map('i', '<ESC>', '<C-C>')
 
 -- Remap add because of tmux
-map('n','<C-a><C-a>','<C-a>')
+map('n', '<C-a><C-a>', '<C-a>')
 
 -- Window sizing
-map('n','<Leader>-','5<C-W><,<CR>')
-map('n','<Leader>+','5<C-W>>,<CR>')
+map('n', '<Leader>-', '5<C-W><,<CR>')
+map('n', '<Leader>+', '5<C-W>>,<CR>')
 
 -- Search for visual selection
 -- TODO: This is invalid...
 -- map('v','//','y/\V<C-R>=escape(@","/\")<CR><CR>')
 
 -- Markdown preview
-map('n','<leader>mp','<Plug>MarkdownPreview')
-map('n','<leader>ms','<Plug>MarkdownPreviewStop')
+map('n', '<leader>mp', '<Plug>MarkdownPreview')
+map('n', '<leader>ms', '<Plug>MarkdownPreviewStop')
 
 -- Python specifip
-map('n','<leader>rp',':w,<CR>,:!python,%<CR>')
+map('n', '<leader>rp', ':w,<CR>,:!python,%<CR>')
 
 -- FZF Fuzzyfinder
-map('n','<leader>f',':Files<CR>')
-map('n','<leader>t',':Ag<CR>')
-map('n','<leader>b',':Buffer<CR>')
-map('n','<leader>fm',':Marks<CR>')
-map('n','<leader>fg',':Commits<CR>')
-map('n','<leader>fw',':Windows<CR>')
+-- map('n','<leader>f',':Files<CR>')
+-- map('n','<leader>a',':Ag<CR>')
+-- map('n','<leader>b',':Buffer<CR>')
+-- map('n','<leader>m',':Marks<CR>')
+-- map('n','<leader>c',':Commits<CR>')
+-- map('n','<leader>w',':Windows<CR>')
 
--- Nerdtree
-map('n','<leader>n',':NERDTreeFocus<CR>')
-map('n','n<C-n>',':NERDTree<CR>')
-map('n','n<C-t>',':NERDTreeToggle<CR>')
-map('n','n<C-f>',':NERDTreeFind<CR>')
+-- Telescope Fuzzyfinder
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', 'ff', builtin.find_files, {})
+vim.keymap.set('n', 'fg', builtin.live_grep, {})
+vim.keymap.set('n', 'fc', builtin.git_commits, {})
+vim.keymap.set('n', 'fb', builtin.buffers, {})
+vim.keymap.set('n', 'fq', builtin.quickfix, {})
+vim.keymap.set('n', 'fr', builtin.registers, {})
+vim.keymap.set('n', 'fm', builtin.marks, {})
+vim.keymap.set('n', 'fh', builtin.help_tags, {})
+
+vim.keymap.set('n', 'gd', builtin.lsp_definitions, bufopts)
+vim.keymap.set('n', 'gr', builtin.lsp_references, bufopts)
+vim.keymap.set('n', 'gi', builtin.lsp_implementations, bufopts)
+vim.keymap.set('n', 'gt', builtin.lsp_type_definitions, bufopts)
+vim.keymap.set('n', 'gs', builtin.lsp_document_symbols, bufopts)
+
+-- NeoTree file explorer
+map('n', '<leader>nn', ':NvimTreeToggle<CR>')
+map('n', '<leader>nf', ':NvimTreeFindFile<CR>')
+map('n', '<leader>nc', ':NvimTreeCollapse<CR>')
 
 -- Debugger
 -- TODO: Check the plugins below, do you need them?
-map('n','t<C-n>',':TestNearest<CR>')
-map('n','t<C-f>',':TestFile<CR>')
-map('n','t<C-t>',':TestSuite<CR>')
-map('n','t<C-l>',':TestLast<CR>')
-map('n','t<C-g>',':TestVisit<CR>')
+map('n', 't<C-n>', ':TestNearest<CR>')
+map('n', 't<C-f>', ':TestFile<CR>')
+map('n', 't<C-t>', ':TestSuite<CR>')
+map('n', 't<C-l>', ':TestLast<CR>')
+map('n', 't<C-g>', ':TestVisit<CR>')
 -- for normal mode - the word under the cursor
-map('n','<Leader>di','<Plug>VimspectorBalloonEval')
+map('n', '<Leader>di', '<Plug>VimspectorBalloonEval')
 -- for visual mode, the visually selected text
-map('x','<Leader>di','<Plug>VimspectorBalloonEval')
+map('x', '<Leader>di', '<Plug>VimspectorBalloonEval')
 -- nmap <Leader>dd <Plug>Vimspector
 
 -- Catkin make ros workspace
@@ -131,5 +147,16 @@ end
 
 vim.keymap.set('n', '<leader>bc', ':lua build_current_catkin_ws()<CR>', { silent = false })
 
-map('t','<ESC>','<C-\\><C-n>')
+map('t', '<ESC>', '<C-\\><C-n>')
 
+function _G.set_terminal_keymaps()
+    local opts = { buffer = 0 }
+    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+    vim.keymap.set('t', '<leader>h', [[<Cmd>wincmd h<CR>]], opts)
+    vim.keymap.set('t', '<leader>j', [[<Cmd>wincmd j<CR>]], opts)
+    vim.keymap.set('t', '<leader>k', [[<Cmd>wincmd k<CR>]], opts)
+    vim.keymap.set('t', '<leader>l', [[<Cmd>wincmd l<CR>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
--- nvim/lua/Hlanden/lsp_and_cmp.lua
+++ nvim/lua/Hlanden/lsp_and_cmp.lua
@@ -1,68 +1,68 @@
 -- Set up nvim-cmp.
-local cmp = require'cmp'
+local cmp = require 'cmp'
 
 cmp.setup({
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-       vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-    end,
-  },
-  window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<Tab>'] = cmp.mapping.select_next_item(),
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    -- { name = 'vsnip' }, -- For vsnip users.
-    -- { name = 'luasnip' }, -- For luasnip users.
-    { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
-  }, {
-    { name = 'buffer' },
-  })
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        end,
+    },
+    window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<Tab>'] = cmp.mapping.select_next_item(),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        -- { name = 'vsnip' }, -- For vsnip users.
+        -- { name = 'luasnip' }, -- For luasnip users.
+        { name = 'ultisnips' }, -- For ultisnips users.
+        -- { name = 'snippy' }, -- For snippy users.
+    }, {
+        { name = 'buffer' },
+    })
 })
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-  }, {
-    { name = 'buffer' },
-  })
+    sources = cmp.config.sources({
+        { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+        { name = 'buffer' },
+    })
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline({ '/', '?' }, {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    })
 })
 
 -- LSP settings and key mappings
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[g', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']g', vim.diagnostic.goto_next, opts)
@@ -71,48 +71,68 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-  vim.keymap.set('n' , 'gD'         , vim.lsp.buf.declaration             , bufopts)
-  vim.keymap.set('n' , 'gd'         , vim.lsp.buf.definition              , bufopts)
-  vim.keymap.set('n' , 'gr'         , vim.lsp.buf.references              , bufopts)
-  vim.keymap.set('n' , 'K'          , vim.lsp.buf.hover                   , bufopts)
-  vim.keymap.set('n' , 'gi'         , vim.lsp.buf.implementation          , bufopts)
-  vim.keymap.set('n' , 'gs'         , vim.lsp.buf.signature_help          , bufopts)
-  vim.keymap.set('n' , 'gt'         , vim.lsp.buf.type_definition         , bufopts)
-  vim.keymap.set('n' , '<leader>wa' , vim.lsp.buf.add_workspace_folder    , bufopts)
-  vim.keymap.set('n' , '<leader>wr' , vim.lsp.buf.remove_workspace_folder , bufopts)
-  vim.keymap.set('n' , '<leader>wl' , function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n' , '<leader>rn' , vim.lsp.buf.rename                                 , bufopts)
-  vim.keymap.set('n' , '<leader>ca' , vim.lsp.buf.code_action                            , bufopts)
-  vim.keymap.set('n' , 'gf'         , function() vim.lsp.buf.format { async = true } end , bufopts)
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<leader>wl', function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', '<leader>=', function() vim.lsp.buf.format { async = true } end, bufopts)
 
-  -- Available mappings:
-  -- map('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
-  -- map('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>')
-  -- map('n','K','<cmd>lua vim.lsp.buf.hover()<CR>')
-  -- map('n','gr','<cmd>lua vim.lsp.buf.references()<CR>')
-  -- map('n','gs','<cmd>lua vim.lsp.buf.signature_help()<CR>')
-  -- map('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
-  -- map('n','gt','<cmd>lua vim.lsp.buf.type_definition()<CR>')
-  -- map('n','<leader>gw','<cmd>lua vim.lsp.buf.document_symbol()<CR>')
-  -- map('n','<leader>gW','<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
-  -- map('n','<leader>ah','<cmd>lua vim.lsp.buf.hover()<CR>')
-  -- map('n','<leader>af','<cmd>lua vim.lsp.buf.code_action()<CR>')
-  -- map('n','<leader>ee','<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>')
-  -- map('n','<leader>ar','<cmd>lua vim.lsp.buf.rename()<CR>')
-  -- map('n','<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>')
-  -- map('n','<leader>ai','<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
-  -- map('n','<leader>ao','<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    -- Available mappings:
+    -- map('n','gD','<cmd>lua vim.lsp.buf.declaration()<CR>')
+    -- map('n','gd','<cmd>lua vim.lsp.buf.definition()<CR>')
+    -- map('n','K','<cmd>lua vim.lsp.buf.hover()<CR>')
+    -- map('n','gr','<cmd>lua vim.lsp.buf.references()<CR>')
+    -- map('n','gs','<cmd>lua vim.lsp.buf.signature_help()<CR>')
+    -- map('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
+    -- map('n','gt','<cmd>lua vim.lsp.buf.type_definition()<CR>')
+    -- map('n','<leader>gw','<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+    -- map('n','<leader>gW','<cmd>lua vim.lsp.buf.workspace_symbol()<CR>')
+    -- map('n','<leader>ah','<cmd>lua vim.lsp.buf.hover()<CR>')
+    -- map('n','<leader>af','<cmd>lua vim.lsp.buf.code_action()<CR>')
+    -- map('n','<leader>ee','<cmd>lua vim.lsp.util.show_line_diagnostics()<CR>')
+    -- map('n','<leader>ar','<cmd>lua vim.lsp.buf.rename()<CR>')
+    -- map('n','<leader>=', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+    -- map('n','<leader>ai','<cmd>lua vim.lsp.buf.incoming_calls()<CR>')
+    -- map('n','<leader>ao','<cmd>lua vim.lsp.buf.outgoing_calls()<CR>')
 end
 
 -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
 require('lspconfig').pyright.setup { capabilities = capabilities, on_attach = on_attach }
-require('lspconfig').clangd.setup  { capabilities = capabilities, on_attach = on_attach }
-require('lspconfig').bashls.setup  { capabilities = capabilities, on_attach = on_attach }
-require('lspconfig').lemminx.setup  { capabilities = capabilities, on_attach = on_attach , filetypes = { "xml", "xsd", "xsl", "xslt", "svg", "urdf", "xacro"}}
+require('lspconfig').clangd.setup { capabilities = capabilities, on_attach = on_attach }
+require('lspconfig').bashls.setup { capabilities = capabilities, on_attach = on_attach }
+require('lspconfig').lemminx.setup { capabilities = capabilities, on_attach = on_attach,
+    filetypes = { "xml", "xsd", "xsl", "xslt", "svg", "urdf", "xacro" } }
+require 'lspconfig'.sumneko_lua.setup {
+    capabilities = capabilities, on_attach = on_attach,
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+}
