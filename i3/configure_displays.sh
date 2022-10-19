e18b4dd364dca1fd5017226776ec7eddce35976c
Jorgen Haaland <jorgen.haaland@wheel.me>
2022-10-19 12:58:40 +0200

    Changes to i3, making it more stable and work with work-computer

---
+++ i3/configure_displays.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# xrandr --output DP-4 --auto --rotate normal --pos 0x0 --output DP-0 --mode 5120x1440 --primary --rotate normal --right-of DP-4 
+# xrandr --output DP-4 --auto --rotate normal --pos 0x0 --output HDMI-0 --mode 3840x1200 --primary --rotate normal --right-of DP-4 
+xrandr --output DP-4 --auto --rotate normal --pos 0x0 
+# xrandr --output DP-4 --auto --primary --output HDMI-0 --right-of DP-4
+sleep 1
