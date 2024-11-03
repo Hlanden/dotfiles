d9dc71608d8a41d7e1724910dbecc01babdede72
Jørgen Haaland <jorgen.haaland@wheel.me>
2024-11-03 21:47:14 +0100

    Debugger looks nvim

--- nvim/lua/Hlanden/settings.lua
+++ nvim/lua/Hlanden/settings.lua
@@ -39,6 +39,8 @@ vim.cmd('colorscheme nightfox')
 -- vim.cmd('colorscheme rose-pine')
 -- vim.cmd('colorscheme gruvbox')
 
+require'colorizer'.setup()
+
 require'tabline'.setup {
     -- Defaults configuration options
     enable = true,
@@ -323,3 +325,13 @@ g.UltiSnipsJumpBackwardTrigger = "<c-k>"
 g.UltiSnipsEnableSnipMate = 0
 
 g.vimtex_view_general_viewer = 'okular'
+
+vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#993939', bg = '#31353f' })
+vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#61afef', bg = '#31353f' })
+vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379', bg = '#31353f' })
+
+vim.fn.sign_define('DapBreakpoint', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
+vim.fn.sign_define('DapBreakpointCondition', { text='ﳁ', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
+vim.fn.sign_define('DapBreakpointRejected', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl= 'DapBreakpoint' })
+vim.fn.sign_define('DapLogPoint', { text='', texthl='DapLogPoint', linehl='DapLogPoint', numhl= 'DapLogPoint' })
+vim.fn.sign_define('DapStopped', { text='', texthl='DapStopped', linehl='DapStopped', numhl= 'DapStopped' })
