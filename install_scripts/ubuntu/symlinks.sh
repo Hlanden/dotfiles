4bb0521fae2cf6f944cd8cc2dec1a2b5775e273d
Jørgen Haaland <jorgen.haaland@outlook.com>
2025-02-22 15:26:57 +0100

    update symlinks

--- install_scripts/ubuntu/symlinks.sh
+++ install_scripts/ubuntu/symlinks.sh
@@ -1,16 +1,13 @@
 # NOTE: Use at own risk. This will overwrite all existing symlinks
 mkdir -p $HOME/.config 
 
-ln -sf $HOME/dotfiles/bash/.bashrc $HOME/.bashrc
-ln -sf $HOME/dotfiles/bash/.bash_profile $HOME/.bash_profile
-ln -sf $HOME/dotfiles/nvim $HOME/.config/nvim
-ln -sf $HOME/dotfiles/tmux $HOME/.config/tmux
-ln -sf $HOME/dotfiles/ranger $HOME/.config/ranger
-ln -sf $HOME/dotfiles/ultisnips $HOME/.config/ultisnips
-ln -sf $HOME/dotfiles/xsession/.xsessionrc $HOME/.xsessionrc
-
-# alacritty
-# TODO: i3
-# TODO: picom
-# TODO: polybar
-# TODO: ultisnips
+ln -s $HOME/dotfiles/zsh/.zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/zsh/.profile $HOME/.profile
+ln -s $HOME/dotfiles/nvim $HOME/.config/nvim
+ln -s $HOME/dotfiles/tmux $HOME/.config/tmux
+ln -s $HOME/dotfiles/ranger $HOME/.config/ranger
+ln -s $HOME/dotfiles/xsession/.xsessionrc $HOME/.xsessionrc
+ln -s $HOME/dotfiles/i3 $HOME/.config/i3
+ln -s $HOME/dotfiles/alacritty $HOME/.config/alacritty
+ln -s $HOME/dotfiles/tmux $HOME/.config/tmux
+ln -s $HOME/dotfiles/polybar $HOME/.config/polybar
