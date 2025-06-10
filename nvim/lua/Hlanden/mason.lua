d1dea4b8103317c30506d4910553f027bd8b8ea2
Jorgen Haaland <jorgen.haaland@outlook.com>
2025-06-10 08:21:17 +0200

    Conditional logic for maon packages baed on architecture

--- nvim/lua/Hlanden/mason.lua
+++ nvim/lua/Hlanden/mason.lua
@@ -1,29 +1,46 @@
 require("mason").setup()
 
+local arch = vim.loop.os_uname().machine
+local is_arm = string.find(arch:lower(), "arm") or string.find(arch:lower(), "aarch64")
+
+local base_packages = {
+    "pyright",
+    "bashls",
+    "rust_analyzer",
+    "kotlin_language_server",
+    "angularls",
+    "biome",
+    "vtsls",
+    "cssls",
+    "html",
+    "ltex",
+    "texlab",
+    "terraformls",
+    "tailwindcss",
+    "robotframework_ls",
+    "lua_ls",
+    "eslint",
+    "harper_ls",
+}
+
+-- Packages that only exists on AMD
+local non_arm_packages = {
+    "clangd",
+    "lemminx",
+    "csharp_ls",
+}
+
+local mason_packages = base_packages
+
+if not is_arm then
+    for _, pkg in ipairs(non_arm_packages) do
+        mason_packages[#mason_packages + 1] = pkg
+    end
+end
+
 require("mason-lspconfig").setup({
-	ensure_installed = {
-		"pyright",
-		"clangd",
-		"bashls",
-		"rust_analyzer",
-		"kotlin_language_server",
-		"angularls",
-		"biome",
-		"vtsls",
-		"cssls",
-		"html",
-		"lemminx",
-		"ltex",
-		"texlab",
-		"csharp_ls",
-		"terraformls",
-		"tailwindcss",
-		"robotframework_ls",
-		"lua_ls",
-		"eslint",
-		"harper_ls",
-	},
-    automatic_enable = false,
+	ensure_installed = mason_packages,
+	automatic_enable = false,
 	automatic_installation = true,
 })
 
