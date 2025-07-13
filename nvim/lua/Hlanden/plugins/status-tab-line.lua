0d9d88da3be9e29c29c0fd8f3fa3f931c69f9eda
Jorgen Haaland <jorgen.haaland@outlook.com>
2025-07-13 22:12:07 +0200

    seperate plugins into individual files

---
+++ nvim/lua/Hlanden/plugins/status-tab-line.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "kyazdani42/nvim-web-devicons" },
+    },
+    {
+        "kdheepak/tabline.nvim",
+        dependencies = {
+            "hoob3rt/lualine.nvim",
+            "kyazdani42/nvim-web-devicons",
+        },
+    }
+}
---
+++ nvim/lua/Hlanden/plugins/telescope.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+    },
+    { "nvim-telescope/telescope-ui-select.nvim" },
+}
