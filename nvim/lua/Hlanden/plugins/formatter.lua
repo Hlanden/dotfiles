5f656d2ed75f8f94d1bd28dfd15d9d4f1fd3dbe8
Joergen Haaland <jorgen.haaland@gjensidige.no>
2025-07-14 13:20:02 +0200

    remove lsp and mason files and move to lsp setup

--- nvim/lua/Hlanden/plugins/formatter.lua
+++ nvim/lua/Hlanden/plugins/formatter.lua
@@ -1,3 +1,73 @@
 return {
-    { "mhartington/formatter.nvim" }
+	{
+		"stevearc/conform.nvim",
+		keys = {
+			{
+				"<leader>=",
+				function()
+					require("conform").format()
+				end,
+				mode = { "n", "v" },
+				desc = "Format Injected Langs",
+			},
+		},
+		opts = {
+			default_format_opts = {
+				async = false,
+				quiet = false,
+				lsp_format = "fallback",
+			},
+			format_on_save = nil,
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "black" },
+				rust = { "rustfmt", "lsp" },
+				javascript = { "prettierd", "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				typescript = { "prettier", "tsfmt" },
+				typescriptreact = { "prettier" },
+				sh = { "shfmt" },
+				terraform = { "terraform_fmt" },
+				cs = { "csharpier" },
+				markdown = { "prettier" },
+				["*"] = { "trim_whitespace" },
+			},
+			formatters = {
+				csharpier = {
+					command = "csharpier",
+					args = { "format", "--write-stdout" },
+					stdin = true,
+				},
+				terraform_fmt = {
+					command = "terraform",
+					args = { "fmt", "-" },
+					stdin = true,
+				},
+				trim_whitespace = {
+					function(ctx)
+						local bufname = vim.api.nvim_buf_get_name(ctx.buf)
+						if string.match(bufname, "octo://") or vim.bo.filetype == "octo" then
+							return nil
+						end
+						return require("conform.formatters.trim_whitespace")
+					end,
+				},
+				prettier = {
+					command = "prettier",
+					args = function(ctx)
+						local args = {
+							"--stdin-filepath",
+							vim.fn.fnameescape(vim.api.nvim_buf_get_name(ctx.buf)),
+						}
+						if vim.bo.filetype == "markdown" then
+							vim.list_extend(args, { "--prose-wrap=always", "--print-width=80" })
+						end
+						return args
+					end,
+					stdin = true,
+				},
+			},
+		},
+	},
 }
--- nvim/lua/Hlanden/plugins/lsp.lua
+++ nvim/lua/Hlanden/plugins/lsp.lua
@@ -1,10 +1,122 @@
 return {
-    { "neovim/nvim-lspconfig" },
-    { "williamboman/mason.nvim" },
-    { "williamboman/mason-lspconfig.nvim" },
     {
-        "WhoIsSethDaniel/mason-tool-installer.nvim",
-        dependencies = { "williamboman/mason.nvim" },
-    },
-    { "Hoffs/omnisharp-extended-lsp.nvim" },
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            {
+                "WhoIsSethDaniel/mason-tool-installer.nvim",
+                dependencies = { "williamboman/mason.nvim" },
+            },
+            "Hoffs/omnisharp-extended-lsp.nvim",
+            "hrsh7th/nvim-cmp"
+        },
+        config = function()
+            local arch = vim.loop.os_uname().machine
+            local is_arm = string.find(arch:lower(), "arm") or string.find(arch:lower(), "aarch64")
+            local base_packages = {
+                "pyright",
+                "bashls",
+                "rust_analyzer",
+                "kotlin_language_server",
+                "angularls",
+                "biome",
+                "vtsls",
+                "cssls",
+                "html",
+                "ltex",
+                "texlab",
+                "terraformls",
+                "tailwindcss",
+                "powershell_es",
+                -- "robotframework_ls",
+                "lua_ls",
+                "eslint",
+                "harper_ls",
+                "omnisharp",
+            }
+
+            -- Packages that only exists on AMD
+            local non_arm_packages = {
+                "clangd",
+                "lemminx",
+            }
+            local mason_packages = base_packages
+
+            if not is_arm then
+                for _, pkg in ipairs(non_arm_packages) do
+                    mason_packages[#mason_packages + 1] = pkg
+                end
+            end
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                ensure_installed = mason_packages,
+                automatic_enable = false,
+                automatic_installation = true,
+            })
+            require("mason-tool-installer").setup({
+                ensure_installed = {
+                    "stylua",
+                    "cpplint",
+                    "black",
+                    "fixjson",
+                    "prettier",
+                    "csharpier",
+                },
+                auto_update = true,
+                run_on_start = true,
+            })
+
+            local cmp_lsp = require("cmp_nvim_lsp")
+            local capabilities = vim.tbl_deep_extend(
+                "force",
+                {},
+                vim.lsp.protocol.make_client_capabilities(),
+                cmp_lsp.default_capabilities())
+
+            require("lspconfig").pyright.setup({ capabilities = capabilities })
+            require("lspconfig").clangd.setup({ capabilities = capabilities })
+            require("lspconfig").bashls.setup({ capabilities = capabilities })
+            require("lspconfig").rust_analyzer.setup({ capabilities = capabilities })
+            require("lspconfig").kotlin_language_server.setup({ capabilities = capabilities })
+            require("lspconfig").angularls.setup({ capabilities = capabilities })
+            require("lspconfig").biome.setup({ capabilities = capabilities })
+            require("lspconfig").vtsls.setup({ capabilities = capabilities })
+            require("lspconfig").cssls.setup({ capabilities = capabilities })
+            require("lspconfig").somesass_ls.setup({ capabilities = capabilities })
+            require("lspconfig").html.setup({ capabilities = capabilities })
+            require("lspconfig").lemminx.setup({
+                capabilities = capabilities,
+                filetypes = { "xml", "xsd", "xsl", "xslt", "svg", "urdf", "xacro" },
+            })
+            require("lspconfig").ltex.setup({ capabilities = capabilities, filetypes = { "tex" } })
+            require("lspconfig").texlab.setup({ capabilities = capabilities, filetypes = { "tex" } })
+            require("lspconfig").omnisharp.setup({
+                capabilities = capabilities,
+                cmd = { "OmniSharp", "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
+                on_attach = function(client, bufnr)
+                    local map = function(mode, lhs, rhs)
+                        vim.keymap.set(mode, lhs, rhs, { buffer = bufnr })
+                    end
+
+                    map("n", "gd", "<cmd>lua require('omnisharp_extended').telescope_lsp_definition()<cr>")
+                    map("n", "<leader>D", "<cmd>lua require('omnisharp_extended').telescope_lsp_type_definition()<cr>")
+                    map("n", "gr", "<cmd>lua require('omnisharp_extended').telescope_lsp_references()<cr>")
+                    map("n", "gi", "<cmd>lua require('omnisharp_extended').telescope_lsp_implementation()<cr>")
+                end,
+                settings = {
+                    RoslynExtensionsOptions = {
+                        enableDecompilationSupport = true,
+                    },
+                },
+            })
+            require("lspconfig").terraformls.setup({ capabilities = capabilities })
+            require("lspconfig").tailwindcss.setup({ capabilities = capabilities })
+            require("lspconfig").robotframework_ls.setup({ capabilities = capabilities })
+            require("lspconfig").eslint.setup({ capabilities = capabilities })
+            require("lspconfig").lua_ls.setup({ capabilities = capabilities })
+            require("lspconfig").harper_ls.setup({ capabilities = capabilities })
+            require("lspconfig").powershell_es.setup({ capabilities = capabilities })
+        end,
+    }
 }
