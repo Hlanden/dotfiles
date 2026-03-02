c506933dc9037e20ce5e9bfc7b2de920d22b0ea9
Jorgen Hlanden <jorgen@hlanden.com>
2026-03-02 14:00:07 +0100

    feat: add watson filetype to render-markdown.nvim

--- nvim/lua/Hlanden/plugins/render-markdown.lua
+++ nvim/lua/Hlanden/plugins/render-markdown.lua
@@ -1,10 +1,11 @@
 return {
-	"MeanderingProgrammer/render-markdown.nvim", -- Make Markdown buffers look beautiful
-	ft = { "codecompanion" },
+	"MeanderingProgrammer/render-markdown.nvim",
+	ft = { "codecompanion", "watson" },
 	opts = {
 		render_modes = true, -- Render in ALL modes
+		file_types = { "markdown", "codecompanion", "watson" },
 		sign = {
-			enabled = false, -- Turn off in the status column
+			enabled = false,
 		},
 		latex = { enabled = false },
 		overrides = {
@@ -12,18 +13,20 @@ return {
 				codecompanion = {
 					html = {
 						tag = {
-							buf = { icon = " ", highlight = "CodeCompanionChatIcon" },
-							file = { icon = " ", highlight = "CodeCompanionChatIcon" },
-							group = { icon = " ", highlight = "CodeCompanionChatIcon" },
-							help = { icon = "󰘥 ", highlight = "CodeCompanionChatIcon" },
-
-							image = { icon = " ", highlight = "CodeCompanionChatIcon" },
-							symbols = { icon = " ", highlight = "CodeCompanionChatIcon" },
-							tool = { icon = "󰯠 ", highlight = "CodeCompanionChatIcon" },
-
-							url = { icon = "󰌹 ", highlight = "CodeCompanionChatIcon" },
+							buf     = { icon = " ",  highlight = "CodeCompanionChatIcon" },
+							file    = { icon = " ",  highlight = "CodeCompanionChatIcon" },
+							group   = { icon = " ",  highlight = "CodeCompanionChatIcon" },
+							help    = { icon = "󰘥 ", highlight = "CodeCompanionChatIcon" },
+							image   = { icon = " ",  highlight = "CodeCompanionChatIcon" },
+							symbols = { icon = " ",  highlight = "CodeCompanionChatIcon" },
+							tool    = { icon = "󰯠 ", highlight = "CodeCompanionChatIcon" },
+							url     = { icon = "󰌹 ", highlight = "CodeCompanionChatIcon" },
 						},
 					},
+				},
+				watson = {
+					-- Watson uses standard markdown headings and code blocks
+					-- No custom html tags needed
 				},
 			},
 		},
